cfba945e91a162b9830bf7f196d0403b5b7456b5
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-22 17:08:01 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//Database
+require("dotenv").config();
+
+const pgp = require("pg-promise")();
+const db = pgp({
+  host: "localhost",
+  port: 5432,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+});
+
+app.use(bodyParser.json());
+app.use("/static", express.static("static"));
+app.set("view engine", "hbs");
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.listen(8080, function() {
+  console.log("Listening on port 8080");
+});
