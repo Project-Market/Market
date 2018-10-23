640f2e3f44841d273756ffb79b5d9b446db1845c
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-23 18:48:15 +0100

    added dropdown and checkbox in filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { renderComponent } from "recompose";
 
 class Filter extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
-}
 
+  render() {
+    return (
+      <div className="filter">
+        <form className="filter-form">
+          <select name="Cuisine">
+            <option value="American" />
+            <option value="Burger" />
+            <option value="Indian" />
+            <option value="Mexican" />
+          </select>
+
+          <input type="checkbox" name="card" value="card">
+            Accepts Card
+          </input>
+        </form>
+      </div>
+    );
+  }
+}
 export default Filter;
