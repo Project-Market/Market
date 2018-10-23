c783f2753aa2bd8cf6dd387382e8c4d1d549d127
Kate <mhq0612@163.com>
2018-10-23 10:04:29 +0100

    initial set up

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "../styles/App.scss";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+    render(){
+    return (
+      <div>
+        Market app
+      </div>
+    )
+  }
+  }
+
+  export default App;
