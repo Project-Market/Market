b6cba4619313aa0fad05494da6f59c06440e6d56
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-23 14:36:53 +0100

    commented out database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-Database
+// Database
 require("dotenv").config();
 
 const pgp = require("pg-promise")();
@@ -20,16 +20,13 @@ app.use(bodyParser.json());
 app.use("/static", express.static("static"));
 app.set("view engine", "hbs");
 
-
 //Get REQ
 app.get("/", function(req, res) {
   res.render("index");
 });
 
 app.get("/api/market_stall", function(req, res) {
-  db.any(
-    `SELECT * FROM market_stall`
-  )
+  db.any(`SELECT * FROM market_stall`)
     .then(function(data) {
       res.json(data);
     })
@@ -52,33 +49,33 @@ app.get("/api/dish", function(req, res) {
 });
 
 app.get("/api/market_stall/with_dish", function(req, res) {
-    db.any(
-      `SELECT *
+  db.any(
+    `SELECT *
       FROM market_stall,dish
       WHERE market_stall.id = dish.market_stall_id `
-    )
-      .then(function(data) {
-        res.json(data);
-      })
-      .catch(function(error) {
-        res.json({ error: error.message });
-      });
-  });
+  )
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(function(error) {
+      res.json({ error: error.message });
+    });
+});
 
-  app.get("/api/market_stall/:id", function(req, res) {
-    const market_stall_id = req.params.id
-    db.any(
-      `SELECT *  FROM market_stall,dish\
-      WHERE id =$1 AND market_stall.id = dish.market_stall_id`,[market_stall_id] 
-    )
-      .then(function(data) {
-        res.json(data);
-      })
-      .catch(function(error) {
-        res.json({ error: error.message });
-      });
-  });
-
+app.get("/api/market_stall/:id", function(req, res) {
+  const market_stall_id = req.params.id;
+  db.any(
+    `SELECT *  FROM market_stall,dish\
+      WHERE id =$1 AND market_stall.id = dish.market_stall_id`,
+    [market_stall_id]
+  )
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(function(error) {
+      res.json({ error: error.message });
+    });
+});
 
 app.listen(8080, function() {
   console.log("Listening on port 8080");
