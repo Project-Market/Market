8edbb709eb75c42b661a173ee20b44edecc94ff7
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-23 16:45:16 +0100

    post review fetch started, need more info

--- src/components/MarketApp.js
+++ src/components/MarketApp.js
@@ -23,9 +23,105 @@ class MarketApp extends React.Component {
       });
   }
 
+  // DATA should == [
+  // {
+  //     id: 0,
+  //     title: "KIN",
+  //     average_rating: 7,
+  //     image: "/img/1.jpg",
+  //     category: "asian",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 1,
+  //     title: "Boom Burger",
+  //     average_rating: 7,
+  //     image: "/img/2.jpg",
+  //     category: "burger",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 2,
+  //     title: "Jamal Shake Shack",
+  //     average_rating: 7,
+  //     image: "/img/3.jpg",
+  //     category: "american",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 3,
+  //     title: "Luke Warm Curry",
+  //     average_rating: 7,
+  //     image: "/img/4.jpg",
+  //     category: "indian",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 4,
+  //     title: "Katsu Kate",
+  //     average_rating: 7,
+  //     image: "/img/5.jpg",
+  //     category: "asian",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 5,
+  //     title: "Tom Yum",
+  //     average_rating: 7,
+  //     image: "/img/6.jpg",
+  //     category: "asian",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     },
+  //     {
+  //     id: 6,
+  //     title: "Burrito Brothers",
+  //     average_rating: 7,
+  //     image: "/img/7.jpg",
+  //     category: "mexican",
+  //     stall_website: "www.kin.com",
+  //     location: "leather_lane",
+  //     takes_card: null,
+  //     lat: null,
+  //     lang: null
+  //     }
+  //     ]
+
+  // POST REVIEW should ==
   // {market_stall_id: 1,user_name: "Chris",rating: 5, review: "sublime"}
 
-  reviewReciever(review) {}
+  submitReviewHandle(review) {
+    fetch("/api/reviews", {
+      method: postMessage,
+      body: JSON.stringify(review),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(data => {});
+  }
 
   render() {
     return;
