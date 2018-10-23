28b19b82cc9b14561435d794c863457085c0983d
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-23 14:35:20 +0100

    added pinpoints

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,28 +1,38 @@
-import React from "react"
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-
+import React from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=this-is-api-key&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=AIzaSyAMzOxPMWZ48_HxfwIoeLu6O0zpNmK2f6U&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
-)((props) => {
+)(props => {
   return (
     <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      defaultZoom={12}
+      defaultCenter={{ lat: 51.520131, lng: -0.109311 }}
     >
-      {props.markerLocations.map( location => {
-        return <Marker key={`${location.lat}${location.lng}`}position={{ lat: location.lat, lng: location.lng }} onClick={props.onMarkerClick} />
+      {props.markerLocations.map(location => {
+        return (
+          <Marker
+            key={`${location.lat}${location.lng}`}
+            position={{ lat: location.lat, lng: location.lng }}
+            onClick={props.onMarkerClick}
+          />
+        );
       })}
     </GoogleMap>
-  )
+  );
 });
 
 export default MyMapComponent;
