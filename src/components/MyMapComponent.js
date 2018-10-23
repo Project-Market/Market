ac3cda5c9a8f1ee430be28db4254a64470b6d266
jamal westfield <jamalwestfield@gmail.com>
2018-10-23 14:31:40 +0100

    added db

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -3,11 +3,12 @@ import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 
+
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=this-is-api-key&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=AIzaSyAMzOxPMWZ48_HxfwIoeLu6O0zpNmK2f6U&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `150px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
