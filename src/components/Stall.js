8c0096639656bd5ccfe34b0f441baf71f38760a4
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-24 15:19:08 +0100

    working

--- src/components/Stall.js
+++ src/components/Stall.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactStars from "react-stars";
 import { render } from "react-dom";
 import StoreDetails from "./StoreDetails";
-import StarRatings from 'react-star-ratings';
+import StarRatings from "react-star-ratings";
 import "../styles/Stall.scss";
 import cx from "classnames";
 
@@ -37,22 +37,19 @@ class Stall extends React.PureComponent {
 
     return (
       <div className="stall__info">
-
         <h3>{this.props.stall.title}</h3>
         <img className="stall__image" src={this.props.stall.image} />
 
-
         <StarRatings
-            rating={Number(this.props.stall.average_rating)}
-            starRatedColor="#0BBC62"
-
-            numberOfStars={5}
-            className='star__rating'
-            starDimension="20px"
-            starSpacing="5px"
-          />
-            <img src={cardlogo} alt={cartlogoalt}/>
-            <h3>{this.props.stall.category}</h3>
+          rating={Number(this.props.stall.average_rating)}
+          starRatedColor="#0BBC62"
+          numberOfStars={5}
+          className="star__rating"
+          starDimension="20px"
+          starSpacing="5px"
+        />
+        <img src={cardlogo} alt={cartlogoalt} />
+        <h3>{this.props.stall.category}</h3>
 
         <button onClick={this.clickStallMore}>more info</button>
 
@@ -64,9 +61,6 @@ class Stall extends React.PureComponent {
       </div>
     );
   }
+}
 
-  
-  export default Stall;
-
-
-
+export default Stall;
--- src/components/StoreDetails.js
+++ src/components/StoreDetails.js
@@ -11,18 +11,14 @@ class StoreDetails extends React.Component {
     };
   }
 
-
-
-      fetchDishes() {
-      fetch(`/api/market_stall/${this.props.stall_id}`)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(data => {
-          this.setState({
-            dishes: data
-          });
-
+  fetchDishes() {
+    fetch(`/api/market_stall/${this.props.stall_id}`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          dishes: data
         });
       });
   }
@@ -31,28 +27,31 @@ class StoreDetails extends React.Component {
     this.fetchDishes();
   }
 
-
-    render() {
-
-      return (
-        <div className={this.props.switcher}>
-           {this.state.dishes.map(dish => {
-
-            return (
-                <div  key={dish.id}>
-                  <p>dish title:{dish.dish_title}</p>
-                  <p>price:£{dish.price}</p>
-                  <p>description:{dish.description}</p>
-                  <hr></hr>
-                </div>
-            )
-          })}
-
+  render() {
+    return (
+      <div className={this.props.switcher}>
+        {this.state.dishes.map(dish => {
+          return (
+            <div key={dish.id}>
+              <p>
+                dish title:
+                {dish.dish_title}
+              </p>
+              <p>
+                price:£
+                {dish.price}
+              </p>
+              <p>
+                description:
+                {dish.description}
+              </p>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
-
 }
 
 export default StoreDetails;
-
