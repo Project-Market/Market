9dfacae6fb3b7877a26d19f26c774b2267576983
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-10-26 14:43:25 +0100

    navbar centered

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 import "../styles/Nav.scss";
 
 class Nav extends React.Component {
-    constructor() {
-      super();
-      this.state = {};
-    }
-  
-    render() {
-      return (
-        <div className="nav">
-        Project Market
-         <div className="nav__logos">
-         <img src="/static/img/logos/burger.png"/>
-         <img src="/static/img/logos/noodles.png"/>
-         <img src="/static/img/logos/pizza.png"/>
-         </div>
-        </div>
-      );
-    }
+  constructor() {
+    super();
+    this.state = {};
   }
-  
-  export default Nav;
-  
+
+  render() {
+    return (
+      <div className="nav-grid">
+        <div className="nav">
+          Project Market
+          <div className="nav__logos">
+            <img src="/static/img/logos/burger.png" />
+            <img src="/static/img/logos/noodles.png" />
+            <img src="/static/img/logos/pizza.png" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
